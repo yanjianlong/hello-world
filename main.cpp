60bd283db9accf8276e46637a165bbc5d7157b21
yanjianlong <yanjianlong@126.com>
2015-01-19 19:32:29 +0800

    update

--- main.cpp
+++ main.cpp
@@ -1,12 +1,27 @@
 #include "stdafx.h"
 #include "test_process.h"
 #include "tcp_process.h"
-int main()
+void TestProcess()
 {
 	CTcpProcess* myProcess = new CTcpProcess("tcp");
 	CTestProcess* mytest = new CTestProcess("test_process");
 	mytest->SetCommonProcess(myProcess);
 	mytest->Process();
 	delete mytest;
+}
+
+void TestStruct()
+{
+	struct sdshdr{
+		int len;
+		int free;
+		char buf[];
+	};	
+	cout << "sizeof:" << sizeof(sdshdr) << endl;
+}
+
+int main()
+{
+	TestStruct();
 	return 0;
 }
