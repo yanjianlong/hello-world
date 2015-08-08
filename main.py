41541b4f9e07b8c9187aee2d994572d5d3ed38ee
yanjianlong1991 <yanjianlong1991@debian.wwww.yanjianlong1991.com>
2015-08-08 17:57:48 +0800

    update

--- main.py
+++ main.py
@@ -7,20 +7,46 @@ import recommend.simple_recommend
 import bs4
 import urllib2
 
-def TestBeautifulSoup():
-	url = "https://www.baidu.com/"
+URL_LIST = ["http://www.lampweb.org/linux/2/6.html",
+			]
+
+URL_LIST = ["http://www.jidi.com/", ]
+RESULT_INDEX = {}
+
+def GetUrl():
+	if URL_LIST:
+		return URL_LIST.pop(0)
+	return None
+
+def InitIndex(url):
 	request = urllib2.Request(url)
 	content = urllib2.urlopen(request).read()
-	print content
-
 	result_dict = bs4.BeautifulSoup(content, 'html.parser')
-	print result_dict.prettify()
+	return result_dict
 
-	print result_dict("script")
+def TestBeautifulSoup():
+	while True:
+		url = GetUrl()
+		if not url or len(RESULT_INDEX) > 20:
+			return
+		print "get link:", url
+		result_dict = InitIndex(url)
+		links = result_dict('a')
+		for cur_link in links:
+			if "href" in dict(cur_link.attrs):
+				cur_url = cur_link.attrs["href"]
+				if cur_url[0:4] == "http" and cur_url not in RESULT_INDEX:
+					URL_LIST.append(cur_url)
+				# else:
+				# 	print "error", cur_url
+		RESULT_INDEX[url] = result_dict
+		# print url, "finish"
 
 def Main():
 	# recommend.simple_recommend.TestRecommend()
 	TestBeautifulSoup()
-
+	print "*****************exist url:"
+	for url in RESULT_INDEX:
+		print url
 if __name__ == "__main__":
 	Main()
